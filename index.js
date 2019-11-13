74dc7a5e1c41a554f241bd328945680534835026
jana15 <37902238+jana15@users.noreply.github.com>
2019-11-13 14:59:49 +0100

    Odovzdat

--- index.js
+++ index.js
@@ -10,35 +10,37 @@ const WMSCapabilities = ol.format.WMSCapabilities;
 
 // podkladova mapa
 var layers = [
-    new TileLayer({
-      source: new OSM()
-    }),
-    //
-    new ImageLayer({
-        //extent suradnice rohu map listu
-      extent: [17.7, 48.8, 18.5, 49.3],
-      source: new ImageWMS({
-        url: 'http://localhost:8080/geoserver/zabcikova/wms?service',
-        params: {'LAYERS': 'zabcikova:cesty'},
-        ratio: 1,
-        serverType: 'geoserver'
-      })
+  new TileLayer({
+    source: new OSM()
+  }),
+  //
+  new ImageLayer({
+    //extent suradnice rohu map listu
+    extent: [17.845079678742593, 48.73220891762924, 17.935758655033737, 48.76464387263053],
+    source: new ImageWMS({
+      url: 'http://localhost:8080/geoserver/zabcikova/wms',
+      params: { 'LAYERS': ['zabcikova:cesta','zabcikova:budovy','zabcikova:el_stlpy',	'zabcikova:vodne_toky_2','zabcikova:ihrisko','zabcikova:luka','zabcikova:el_vedenie']},
+      ratio: 1,
+      serverType: 'geoserver'
     })
-  ];
-  var map = new Map({
-    layers: layers,
-    target: 'map',
-    view: new View({
-      projection: 'EPSG:4326',
-      center: [17.886307, 48.743136],
-      zoom: 13
-    })
-  });
-  var parser = new WMSCapabilities();
-  function visibility(){
-  fetch('https://zbgisws.skgeodesy.sk/zbgis_administrativne_hranice_wms_featureinfo/service.svc/get?request=getcapabilities&service=wms').then(function(response) {
+})
+]
+var map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    projection: 'EPSG:4326',
+    center: [17.886307, 48.743136],
+    zoom: 13
+  })
+});
+function visibility() {
+   fetch('http://localhost:8080/geoserver/zabcikova/wms?service=WMS&version=1.3.0&request=GetCapabilities',{mode: 'cors'}).then(function (response) {
     return response.text();
-  }).then(function(text) {
-    var result = parser.read(text);
-    document.getElementById('log').innerText = JSON.stringify(result, null, 2);
-  })};
+    console.log(text)
+  }).then(function (data) {
+    let parser = new DOMParser();
+    XMLDocument = parser.parseFromString(data, 'text/xml');
+    document.getElementById('log').textContent = data;
+  })
+}
